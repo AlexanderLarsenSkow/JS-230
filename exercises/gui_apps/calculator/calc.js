3f532acb4a5f848e7f1750dbe02453134f4235cf
Alex <askow2015@gmail.com>
2025-11-19 15:27:18 -0700

    Added basic logic for interacting with calculator app for entry and operation windows

---
+++ exercises/gui_apps/calculator/calc.js
@@ -0,0 +1,164 @@
+/*
+  Rules:
+    Calc has two parts:
+
+      screen that has a lower part:
+        - entry window, the number being entered OR the result
+        - upper part, the operation window, shows the operation in progress.
+
+      Calc Digits:
+        - 0 - 9
+        - . + - / * % NEG C CE
+        - NEG converts to a negative or vica versa
+        - CE clears entry window and replaces with 0
+        - C button clears both entry and operation window and leaves 0 in entry window.
+
+      Operation Logic Rules:
+        - When user clicks digit button, add number to entry. If 0, replace 0 with the digit
+        - When user clicks operator button, copy current entry and operator into operation window
+          - set entry window to 0
+        - when clicking = button, use the operation window and current entry to calculate the final result.
+
+
+   Example:
+    - 10 + 10 * 5 - 3
+    
+    => 10 + 10 => 20
+      20 * 5 => 100
+      100 - 3 => 97
+
+      accumulator => 10 => 20 => 100 => 97
+
+Data Structure:
+  - create a data structure like this: ['10 + 10', ' * 5', ]
+
+Plan:
+  addToEntryWindow
+  addToOperationWindow
+  clearEntryWindow
+  clearOperationWindow
+
+  neg => place a - in front of the number.
+
+
+
+  [10, 10, 5, 3]
+  operators ['+', '*', '-']
+*/
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const subtract = (a, b) => a - b;
+const divide = (a, b) => a / b;
+
+function determineOperation(operatorString, a, b) {
+  switch(operatorString) {
+    case '+':
+      return add(a, b);
+    case 'x':
+      return multiply(a, b);
+    case '-':
+      return subtract(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
+
+// console.log(determineOperation('*', 10, 10));
+
+// let numbers = [10, 10, 5, 3];
+// let operators = ['+', '*', '-'];
+
+// let firstNumber = numbers.shift();
+// console.log(firstNumber, numbers);
+
+// console.log(numbers.reduce((acc, number, index) => {
+//   let operator = operators[index];
+
+//   return determineOperation(operator, acc, number);
+// }, firstNumber));
+
+// Plan:
+//   addToEntryWindow
+//   addToOperationWindow
+//   clearEntryWindow
+//   clearOperationWindow
+//   neg => place a - in front of the number.
+
+class Calculator {
+  constructor(operation, entry) {
+    this.equation = operation + ` ${entry}`;
+    console.log(this.equation);
+  }
+}
+
+class CalculatorInteractions {
+  constructor() {
+    this.entry = '0';
+    this.operation = '';
+  }
+
+  solve() {
+    const calculator = new Calculator(this.operation, this.entry);
+    // this.entry = String(calculator.calculate());
+  }
+
+  addToEntryWindow(digit) {
+    this.entry === '0' ? this.entry = digit : this.entry += digit;
+  }
+
+  addToOperationWindow(operator) {
+    let space = this.operation ? ' ' : '';
+
+    this.operation += `${space}${this.entry} ${operator}`;
+    this.entry = '0';
+  }
+
+  clearEntryWindow() {
+    this.entry = '0';
+  }
+
+  clearOperationWindow() {
+    this.operation = '';
+  }
+
+  reverseSign() {
+    let number = Number(this.entry);
+    let reverse = -number;
+    this.entry = String(reverse);
+  }
+ }
+
+ let calc = new CalculatorInteractions();
+ calc.addToEntryWindow('1');
+ console.log(calc.entry == '1');
+ calc.addToEntryWindow('1');
+ console.log(calc.entry === '11');
+ calc.addToEntryWindow('1');
+ console.log(calc.entry === '111');
+ 
+calc.addToOperationWindow('+');
+console.log(calc.operation === '111 +');
+console.log(calc.entry === '0');
+
+calc.addToEntryWindow('6');
+calc.reverseSign();
+console.log(calc.entry === '-6');
+
+calc.reverseSign();
+console.log(calc.entry === '6');
+
+calc.addToOperationWindow('*');
+console.log(calc.operation);
+
+calc.addToEntryWindow('1');
+calc.addToEntryWindow('0');
+console.log(calc.entry === '10');
+
+calc.solve();
+
+// calc.solve();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('hello');
+// });
